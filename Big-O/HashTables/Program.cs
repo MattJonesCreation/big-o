f78f5eae090296e9189334f7cd5d7f6f58f995f4
Matt Jones <mjones@softwaredesignsolutions.com>
2019-03-20 15:55:33 -0400

    Finalizing HashTables presentation

--- Big-O/HashTables/Program.cs
+++ Big-O/HashTables/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,61 @@ namespace HashTables
     {
         static void Main(string[] args)
         {
+            //TestingHashingAlgorithms();
+            int size = 10000;
+            int[] arr = new int[size];
+            Dictionary<int, int> hashTable = new Dictionary<int, int>();
+
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = i;
+                hashTable.Add(i, i);
+            }
+
+            // Search for int.max 
+            int key = int.MaxValue;
+            int asset = 0; 
+
+            // Inefficient: O(n)
+            var watch = Stopwatch.StartNew();
+            for (int i = 0; i < size; i++)
+            {
+                asset = arr.FirstOrDefault(x => x == key);
+                asset = i * asset;
+            }          
+            watch.Stop();
+            long ineffTime = watch.ElapsedMilliseconds;
+
+            // Linq: O(lg n)
+            watch = Stopwatch.StartNew();           
+            for (int i = 0; i < size; i++)
+            {
+                asset = Array.BinarySearch(arr, key);
+                asset = i * asset;
+            }
+            watch.Stop();
+            long ineffLinqTime = watch.ElapsedMilliseconds;
+
+            // Efficient: O(1)
+            watch = Stopwatch.StartNew();           
+            for (int i = 0; i < size; i++)
+            {
+                asset = hashTable.ContainsKey(key) ? hashTable[key] : -1;
+                asset = i * asset;
+            }
+            watch.Stop();
+            long effTime = watch.ElapsedMilliseconds;
+
+            Console.WriteLine($"Array Size: {size}");
+            Console.WriteLine($"Algorithm Type  | Algorithm Run Time ");
+            Console.WriteLine($"  Linq Search   |  {ineffTime}ms ");
+            Console.WriteLine($" Binary Search  |  {ineffLinqTime}ms ");
+            Console.WriteLine($"   Dictionary   |  {effTime}ms ");
+            Console.ReadLine();
+        }
+
+        private static void TestingHashingAlgorithms()
+        {
             string input = string.Empty;
 
             while (!input.Equals("quit", StringComparison.OrdinalIgnoreCase))
@@ -19,9 +75,6 @@ namespace HashTables
 
                 Console.WriteLine("Additive: {0}", AdditiveHash(input));
                 Console.WriteLine("DJB2: {0}", Djb2(input));
-            }
-            {
-
             }
         }
 
